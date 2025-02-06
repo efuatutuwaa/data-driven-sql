5bb144e9fb2ef400af9277fedf6ba45342f7ac86
Afua <etutuwaa.88@gmail.com>
2025-02-07 01:59:55 +0200

    refactored 'data_cleaning.py'

--- data_cleaning_scripts/data_cleaning.py
+++ data_cleaning_scripts/data_cleaning.py
@@ -9,7 +9,7 @@ class CountryDataCleaner:
         """Loads name corrections from a JSON file."""
         try:
             base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-            corrections_file = os.path.join(base_dir, "data", "name_corrections.json")
+            corrections_file = os.path.join(base_dir, "data", "countries_corrections.json")
             with open(corrections_file, "r") as f:
                 return json.load(f)
 
@@ -27,3 +27,30 @@ class CountryDataCleaner:
             print("Name corrections is not a valid dictionary. Using original names.")
             return country_name
         return self.name_corrections.get(country_name, country_name)
+
+
+class CityDataCleaner:
+    def __init__(self):
+        self.name_corrections = self.load_city_corrections()
+
+    def load_city_corrections(self):
+        """Loads city corrections from a JSON file."""
+        try:
+            base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            corrections_file = os.path.join(base_dir, "data", "cities_corrections.json")
+            with open(corrections_file, "r") as f:
+                return json.load(f)
+        except FileNotFoundError:
+            print("City corrections file not found. Proceeding without corrections.")
+            return {}
+        except json.JSONDecodeError as err:
+            print(f"Error decoding JSON file: {err}. Proceeding without corrections.")
+            return {}
+
+    def clean_city_name(self, city_name):
+        """Corrects city name based on the city_name corrections."""
+        if not isinstance(self.name_corrections, dict):
+            print("City corrections is not a valid dictionary. Using original names.")
+            return city_name
+        return self.name_corrections.get(city_name, city_name)
+
